93b8e5e487bfe459de733ddfc053d059ffc96728
Aditya Sharma <aditya.sharma4231.in@gmail.com>
2025-07-22 10:08:53 +0530

    feat: add RoleCards component and integrate it into LandingPage; update package dependencies

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -2,12 +2,14 @@
 import Homepage from '../Components/HeroSection'
 import HeroSection2 from '../Components/HeroSecton2'
 import Nav from '../Components/Nav'
+import RoleCards from '../Components/RoleSection'
 
 const LandingPage = () => {
   return (
     <div>
       <Nav/>
       <HeroSection2/>
+      <RoleCards/>
       {/* <Homepage/> */}
     </div>
   )
